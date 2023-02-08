8b9cb76778eb09d984c12f7597fd964da2ec1a9c
Tonyhoang2102 <123832450+Tonyhoang2102@users.noreply.github.com>
2023-02-07 18:42:44 -0800

    fix:  app.js - return to paragraph

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import ImageCropper from './ImageCropper';
 
 function App() {
   return (
-    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-  <path d="M20 60 C 40 20, 60 20, 80 60 S 60 100, 20 60" fill="#FFA07A"/>
-  <path d="M25 65 C 35 25, 65 25, 75 65 S 65 105, 25 65" fill="#FFC107"/>
-  <path d="M30 70 C 40 30, 70 30, 80 70 S 70 110, 30 70" fill="#FFA07A"/>
-</svg>
+  <p>this is</p>
 
   
 
